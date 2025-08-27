50e68a3cfa87a316b9ffe58c06cb9bc5e452ec79
fek247 <ngovantaino2000@gmail.com>
2025-08-27 06:09:49 +0000

    Respond to PING #rg2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_response(_stream)
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_response(mut stream: TcpStream) {
+    let buf = "+PONG\r\n".as_bytes();
+    let _ = stream.write(&buf);
 }
