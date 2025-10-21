20f3fb79a63b48d3860628fb09a1cdce59f4724c
fek247 <ngovantaino2000@gmail.com>
2025-10-21 06:37:59 +0000

    feat: expiry option

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,18 @@ use std::io::{BufRead, BufReader, Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::{Duration, SystemTime};
+
+use tokio::sync::Notify;
 
 const CRLF_TERMINATOR_LEN: usize = 2;
 
 const SUPPORT_COMMANDS: [&str; 4] = ["ping", "echo", "set", "get"];
 
+const SUPPORT_OPTION: [&str; 8] = ["ex", "px", "exat", "pxat", "nx", "xx", "keepttl", "get"];
+
 struct AppState {
-    db: Arc<Mutex<DB>>
+    db: Arc<Mutex<DB>>,
 }
 
 impl AppState {
@@ -23,29 +28,46 @@ impl AppState {
 }
 
 struct DB {
-    entries: HashMap<String, String>,
+    entries: HashMap<String, Entry>,
     pub_sub: HashMap<String, String>,
+    background_task: Notify,
 }
 
 impl DB {
     pub fn new() -> Self {
         let db = DB {
             entries: HashMap::new(),
-            pub_sub: HashMap::new()
+            pub_sub: HashMap::new(),
+            background_task: Notify::new(),
         };
         db
     }
 
-    pub fn set(&mut self, key: &String, value: &String) {
-        self.entries.insert(key.to_string(), value.to_string());
+    pub fn set(&mut self, key: &String, entry: Entry) {
+        self.entries.insert(key.to_string(), entry);
     }
 
     pub fn get(&mut self, key: &String) -> String {
         let value = self.entries.get(key);
         match value {
-            Some(val) => val.to_string(),
+            Some(val) => {
+                match val.expires_at {
+                    Some(expires_at) => {
+                        if (expires_at < SystemTime::now()) {
+                            return String::new();
+                        }
+
+                        return val.value.to_string();
+                    },
+                    None => val.value.to_string(),
+                }
+            },
             None => String::new(),
         }
+    }
+
+    pub fn remove(&mut self, key: &String) -> Option<Entry> {
+        self.entries.remove(key)
     }
 }
 
@@ -85,7 +107,8 @@ fn handle_response(mut stream: TcpStream, map: Arc<Mutex<DB>>) {
             Ok(mut command) => {
                 let mut entries = map.lock().unwrap();
                 if command.name == "set" {
-                    entries.set(&command.key, &command.value);
+                    let entry = Entry { value: command.value.clone(), expires_at: command.expires_at };
+                    entries.set(&command.key, entry);
                 }
                 if command.name == "get" {
                     command.value = entries.get(&command.key);
@@ -109,6 +132,7 @@ fn command_parser(buf: [u8; 512]) -> Result<Command, CommandParserErr>  {
         key: String::new(),
         value: String::new(),
         option: None,
+        expires_at: None,
     };
 
     for i in 0..number_of_elements {
@@ -131,6 +155,31 @@ fn command_parser(buf: [u8; 512]) -> Result<Command, CommandParserErr>  {
 
             if i == 2 {
                 command.value = value.clone();
+            }
+
+            if i == 3 {
+                let opt = command.option.get_or_insert_with(|| CommandOption { key: String::new(), value: String::new() });
+                if !SUPPORT_OPTION.contains(&value.as_str()) {
+                    return Err(CommandParserErr::InvalidOption)
+                }
+                opt.key = value.clone();
+            }
+
+            if i == 4 {
+                let opt = command.option.get_or_insert_with(|| CommandOption { key: String::new(), value: String::new() });
+                let value_i = match value.parse::<u64>() {
+                    Ok(v) => v,
+                    Err(e) => {
+                        return Err(CommandParserErr::InvalidOption);
+                    }
+                };
+                if opt.get_key() == "ex" {
+                    command.expires_at = Some(SystemTime::now() + Duration::from_secs(value_i));
+                }
+                if opt.get_key() == "px" {
+                    command.expires_at = Some(SystemTime::now() + Duration::from_millis(value_i));
+                }
+                opt.value = value.clone();
             }
         }
 
@@ -166,6 +215,7 @@ fn read_length(buf: &[u8], index: &mut usize) -> Result<usize, &'static str> {
 enum CommandParserErr {
     InvalidFormat,
     UnknowCommand,
+    InvalidOption,
 }
 
 struct Command {
@@ -173,6 +223,7 @@ struct Command {
     key: String,
     value: String,
     option: Option<CommandOption>,
+    expires_at: Option<SystemTime>,
 }
 
 struct CommandOption {
@@ -180,14 +231,35 @@ struct CommandOption {
     value: String,
 }
 
+struct Entry {
+    value: String,
+    expires_at: Option<SystemTime>,
+}
+
 impl Command {
     pub fn response(&self) -> String {
         match self.name.as_str() {
             "ping" => String::from("+PONG\r\n"),
             "echo" => format!("${}\r\n{}\r\n", self.key.len(), self.key),
-            "get"  => format!("${}\r\n{}\r\n", self.value.len(), self.value),
+            "get"  => {
+                if self.value.is_empty() {
+                    return String::from("$-1\r\n");
+                }
+
+                format!("${}\r\n{}\r\n", self.value.len(), self.value)
+            },
             "set"  => String::from("+OK\r\n"),
             _      => String::from("-ERR unknown command\r\n"),
         }
+    }
+}
+
+impl CommandOption {
+    pub fn get_key(&self) -> String {
+        self.key.clone()
+    }
+
+    pub fn get_value(&self) -> String {
+        self.value.clone()
     }
 }
